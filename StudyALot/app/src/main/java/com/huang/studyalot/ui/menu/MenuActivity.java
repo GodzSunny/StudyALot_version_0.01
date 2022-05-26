01bd8d029e8ceff64ab21f5df68b82304409b05e
Sunny <81039445+GodzSunny@users.noreply.github.com>
2022-05-25 20:13:32 -0400

    Add files via upload

---
+++ StudyALot/app/src/main/java/com/huang/studyalot/ui/menu/MenuActivity.java
@@ -0,0 +1,17 @@
+package com.huang.studyalot.ui.menu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.huang.studyalot.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+    }
+
+}
